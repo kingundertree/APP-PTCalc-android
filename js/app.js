8890c5a279bf089172c64a5d000aed50b99cab8b
kingundertree <kingundertree@163.com>
2013-05-07 11:04:38 +0800

    部分修改，overshrow功能，兼容低端机

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-
-// 路由配置
-// var jsq = angular.module('jsqpage',[]);
-
-// jsq.config(function($routeProvider) {
-//   $routeProvider.
-//       when('/first', {templateUrl: 'view/nav.html',controller: 'con'}).
-//       otherwise({redirectTo: '/first'});
-// });
-
 var jsq = angular.module('jsqpage',[]);
 
 function con($scope, calculator, buyCalculation, $http, indeedTax, businessTax, sellCalculation){
